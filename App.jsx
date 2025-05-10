2d5a710e95eec0f365d94e8412e0f2026197dc93
meerapandi <meerapandi27@gmail.com>
2025-05-10 12:53:20 +0530

    initial upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import React, { useEffect, useState } from 'react'
+import Home from './components/Home'
+import Opening from './components/Opening'
+function App() {
+  const [isSplashVisible, setSplashVisible] = useState(true);
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setSplashVisible(false);
+    },7570)
+    return ()=> clearTimeout(timer)
+  },[])
+  return (
+   <>
+   {isSplashVisible && <Opening/>}
+   <Home/>
+   </>
+  )
+}
+
+export default App
